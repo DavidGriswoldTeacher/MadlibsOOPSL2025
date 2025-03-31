da8d16f1259da9623ff2972818772ff8c8884bdd
David Griswold <david.griswold@graded.br>
2025-03-31 08:25:02 -0300

    SL Thursday

--- src/App.java
+++ src/App.java
@@ -6,10 +6,38 @@ public class App {
      * Where the program begins!
      */
     public void start() {
-        // myMadlib = readMadlib("madlib.txt");
+        myMadlib = readMadlib("madlibe.txt");
         // promptForAnswers();
         // printMadlib();
     }
+
+    /**
+     * Read the file with the madlib and create a Madlib object
+     * @return a new Madlib objct
+     */
+    public Madlib readMadlib(String filename) {
+        Madlib m = new Madlib();
+        try {
+            io.openFile(filename);
+        } catch (Exception e) {
+            io.output(e.toString());
+            return m;
+        }
+        
+        while (io.fileHasNextLine()) {
+            String nl = io.getNextLine();
+            io.output(nl);
+        }
+
+        return m;
+    }
+
+
+
+
+
+
+
     
     public static void main(String[] args) throws Exception {
         new App().start();
