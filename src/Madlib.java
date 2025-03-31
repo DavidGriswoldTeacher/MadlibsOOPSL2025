a9cf2147cf7b8a62b8151e2605ea0927740e9b4b
David Griswold <david.griswold@graded.br>
2025-03-31 08:25:02 -0300

    SL Thursday

--- src/Madlib.java
+++ src/Madlib.java
@@ -1,3 +1,30 @@
 public class Madlib {
-    // lets finish this together!
+    
+    private String[] questions;
+    private String[] answers;
+    private String[] storyPieces;
+    private int numQuestions = 0;
+    private int numAnswers = 0;
+    private int numStoryPieces = 0;
+
+    public Madlib() {
+        questions = new String[100];
+        answers = new String[100];
+        storyPieces = new String[101];
+    }
+
+    public void addQuestion(String q) {
+        questions[numQuestions] = q;
+        numQuestions++;
+    }
+
+    public void addAnswer(String a) {
+        answers[numAnswers] = a;
+        numAnswers++;
+    }
+
+    public void addStoryPiece(String sp) {
+        storyPieces[numStoryPieces] = sp;
+        numStoryPieces++;
+    }
 }
